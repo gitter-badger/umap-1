9ea03b357bbfb534d8636544b37b675f2b6a6ca9
bsharet <bsharet@cisco.com>
2016-02-15 09:43:20 +0200

    modify prints in the mutable wrapper

--- devices/wrappers.py
+++ devices/wrappers.py
@@ -1,22 +1,21 @@
 import traceback
 
+
 def mutable(stage):
     def wrap_f(func):
         def wrapper(self, *args, **kwargs):
-            print('[-] in')
             data = kwargs.get('fuzzing_data', None)
             response = self.get_mutation(stage=stage, data=data)
             if response:
                 print('[+] Got mutation for stage %s' % stage)
             else:
-                print('[*] Calling %s' % func.__name__)
+                print('[*] Calling %-30s "%s"' % (func.__name__, stage))
                 try:
                     response = func(self, *args, **kwargs)
                 except Exception as e:
                     print(traceback.format_exc())
                     print(''.join(traceback.format_stack()))
                     raise e
-            print('[-] out')
             return response
         return wrapper
     return wrap_f
